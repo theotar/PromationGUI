78e9c1c9b3b4998b05ad04609384b5ac08c0baac
Wojciech Pierzchalski <wojciech.pierzchalski@gmail.com>
2014-07-14 11:32:05 +0200

    START

---
+++ ProMationGUI/src/pl/progree/promation/gui/desktop/PromationGUI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pl.progree.promation.gui.desktop;
+
+/**
+ * @author Progree
+ *
+ */
+public class PromationGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

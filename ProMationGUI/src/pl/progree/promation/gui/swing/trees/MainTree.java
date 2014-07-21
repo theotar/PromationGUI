c84240a0aa30e55af9eb5936aa2ba66f894f2287
Wojciech Pierzchalski <ric@onet.eu>
2014-07-21 23:23:23 +0200

    Zmiana popupmenu działa

--- ProMationGUI/src/pl/progree/promation/gui/swing/trees/MainTree.java
+++ ProMationGUI/src/pl/progree/promation/gui/swing/trees/MainTree.java
@@ -18,6 +18,7 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import pl.progree.promation.Promation;
 import pl.progree.promation.gui.desktop.PromationGUI;
 import pl.progree.promation.gui.desktop.windows.MainWindow;
+import pl.progree.promation.projekt.Projekt;
 
 /**
  * @author Wojciech Pierzchalski, Progree
@@ -31,6 +32,7 @@ public class MainTree extends JTree {
 	private static final long serialVersionUID = 1L;
 	private PromationGUI promationGUI;
 	private JPopupMenu promationMenu;
+	private JPopupMenu projektMenu;
 	
 	public MainTree(PromationGUI promation) {
 		super(new MainTreeModel(promation));
@@ -42,12 +44,17 @@ public class MainTree extends JTree {
 			public void valueChanged(TreeSelectionEvent e) {
 				DefaultMutableTreeNode node=(DefaultMutableTreeNode) e.getPath().getLastPathComponent();
 				if(node.isRoot()) MainTree.this.setComponentPopupMenu(MainTree.this.promationMenu);
-				else  MainTree.this.setComponentPopupMenu(null);
+				else if(node.getUserObject() instanceof Projekt) MainTree.this.setComponentPopupMenu(MainTree.this.projektMenu);
+				else MainTree.this.setComponentPopupMenu(null);
 				
 			}
 		});
 	}
 	private void initMenu(){
+		this.initPromationMenu();
+		this.initProjektMenu();
+	}
+	private void initPromationMenu(){
 		this.promationMenu=new JPopupMenu("Promation");
 		JMenuItem miNowy=new JMenuItem("Nowy Projekt");
 		miNowy.addActionListener(new ActionListener() {
@@ -58,6 +65,18 @@ public class MainTree extends JTree {
 			}
 		});
 		this.promationMenu.add(miNowy);
+	}
+	private void initProjektMenu(){
+		this.projektMenu=new JPopupMenu("Projekt");
+		JMenuItem miZapisz=new JMenuItem("Zapisz");
+		miZapisz.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MainTree.this.promationGUI.getMainWindow().nowyProjekt();
+			}
+		});
+		this.projektMenu.add(miZapisz);
 	}
 
 	public JPopupMenu getPromationMenu() {
